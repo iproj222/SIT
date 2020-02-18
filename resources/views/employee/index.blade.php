385b1c60d46d2797d64b47ce05650c415713f9d9
Kim JongHun <iproj222@gmail.com>
2020-02-18 15:13:18 +0900

    nothing

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -9,6 +9,50 @@ If you want to learning about Laravel framework so please see a laravel website.
     @foreach ($employees as $emp)
         <li>{{$emp->name}}</li>
     @endforeach
+
+    @foreach ($leaves as $leave)
+        <li>{{$leave->reason_note}}</li>
+    @endforeach
 </ol>
+
+<?php
+//setting header to json
+header('Content-Type: application/json');
+
+//database
+define('DB_HOST', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'gpbl2019');
+define('DB_NAME', 'leaves');
+
+//get connection
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+  die("Connection failed: " . $mysqli->error);
+}
+
+//query to get data from the table
+$query = sprintf("SELECT playerid, score FROM score ORDER BY playerid");
+
+//execute query
+$result = $mysqli->query($query);
+
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+  $data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+//now print the data
+print json_encode($data);
+?>
+
 </body>
 </html>
