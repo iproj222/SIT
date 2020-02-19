22bbfcf551e319bf41c45e47fd770e916b1a7602
Kim JongHun <iproj222@gmail.com>
2020-02-19 10:06:17 +0900

    a

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -10,8 +10,8 @@ If you want to learning about Laravel framework so please see a laravel website.
         <li>{{$emp->name}}</li>
     @endforeach -->
 </ol>
-
 <canvas id="myChart" width="400" height="400"></canvas>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 <script>
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
