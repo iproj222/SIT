b8c8b77e50c9e5778937310114a5e37b524fb5ce
Kim JongHun <iproj222@gmail.com>
2020-02-25 11:08:07 +0900

    color change

--- storage/framework/views/d85de4dfa7331a0e5cd92e5ba3cf4f31c9e771b0.php
+++ storage/framework/views/d85de4dfa7331a0e5cd92e5ba3cf4f31c9e771b0.php
@@ -11,7 +11,6 @@
         width : 600px !important;
         height : 600px !important;
     } */
-
 </style>
 
 <h1> Pie Chart</h1>
@@ -34,6 +33,22 @@
 ?>
 
 <script>
+    var coloR = [];
+    var points = new Array(100);
+
+    var dynamicColors = function() {
+        var r = Math.floor(Math.random() * 255);
+        var g = Math.floor(Math.random() * 255);
+        var b = Math.floor(Math.random() * 255);
+        var a = Math.floor(Math.random() * 100);
+        return "rgba(" + r + "," + g + "," + b + ","+ a +")";
+    };
+    
+    window.onload = function(){
+        for (var i=0 ; i < 100 ; i++) 
+            coloR.push(dynamicColors());
+    }
+
     var gctx = document.getElementById("genderChart");
     var genderChart = new Chart(gctx, {
         type: 'pie',
@@ -52,34 +67,64 @@
                             echo "'".$p['count(gender)']."', ";
                         }?>
                     ],
-                    backgroundColor: [
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255,99,132,1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255,99,132,1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
+                    backgroundColor: [  dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                        ],
+                    borderColor: [coloR],
                     borderWidth: 1
                 }]
         },
@@ -110,32 +155,78 @@
                         }?>
                     ],
                     backgroundColor: [
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255,99,132,1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
+                                        dynamicColors(),
                     ],
                     borderColor: [
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255,99,132,1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        coloR
                     ],
                     borderWidth: 1
                 }]
